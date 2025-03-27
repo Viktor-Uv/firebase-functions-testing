31e78a3428e83412ed9c1941ac138347e34527c4
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2025-03-27 15:03:55 +0100

    Add secret SUPER_SECRET

--- functions/src/shared/secrets.ts
+++ functions/src/shared/secrets.ts
@@ -1,4 +1,6 @@
-import {defineString} from "firebase-functions/params";
+import {defineString, defineSecret} from "firebase-functions/params";
 
 export const appId = defineString("APPLICATION_ID");
 export const serviceId = defineString("SERVICE_ID");
+
+export const superSecret = defineSecret("SUPER_SECRET");
